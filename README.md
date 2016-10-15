d2449ec95ee4edef36f87de8f57475478bcaffa2
Frank Wiles <frank@revsys.com>
2016-10-15 18:40:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# {{ cookiecutter.project_name }}
+
+## Overview
+
+## Local Development Setup
+
+All REVSYS projects going forward will use Python 3.5. Make a Python 3.5.x virtualenv and install the dependencies:
+
+    pip install -r requirements.txt
+
+Copy env.template to .env and adjust values to match your local environment:
+
+    cp env.template .env
+
+## Running the tests
+
+To run the tests simply execute:
+
+    py.test
+
+## Deploying
+
+TDB
+
+## Production Environment Considerations
+
+TDB
+
---
+++ {{ cookiecutter.project_dir }}/settings/prod.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = False
+
+# Only use strong password validators in prod
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
