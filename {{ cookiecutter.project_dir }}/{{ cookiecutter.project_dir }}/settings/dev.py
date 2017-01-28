8cf2ba239625947b3a56d1b476fbe0923197624e
Frank Wiles <frank@revsys.com>
2017-01-28 10:42:09 -0600

    Fix initial static files setup

--- {{ cookiecutter.project_dir }}/{{ cookiecutter.project_dir }}/settings/dev.py
+++ {{ cookiecutter.project_dir }}/{{ cookiecutter.project_dir }}/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_deploy')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
