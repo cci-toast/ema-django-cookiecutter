1a06e2a0b646c37c62d01f9f23556ba49a6b4388
Frank Wiles <frank@revsys.com>
2018-07-12 08:30:23 -0500

    Starting move to new structure

--- {{ cookiecutter.project_dir }}/{{ cookiecutter.project_dir }}/settings/dev.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_deploy')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- {{ cookiecutter.project_dir }}/{{ cookiecutter.project_dir }}/settings/test.py
+++
@@ -1,6 +0,0 @@
-from .dev import *
-
-# Makes tests go a bit faster by using a fast password hasher
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-]
