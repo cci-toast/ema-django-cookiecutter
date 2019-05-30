c3afc55656d05897a77a1c6f42936fe86e22c4b2
Frank Wiles <frank@revsys.com>
2019-05-30 10:09:03 -0500

    Fix cookiecutter for deployments

--- {{ cookiecutter.project_dir }}/config/settings.py
+++ {{ cookiecutter.project_dir }}/config/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = env("STATIC_URL", default="/static/")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Swagger Settings
