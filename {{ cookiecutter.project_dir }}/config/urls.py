69bb14d323829d817ed5c3eb79758d6b369cb3a8
Flavio Curella <48334484+fcurella-emoney@users.noreply.github.com>
2019-08-27 14:34:09 -0500

    Update urls.py

--- {{ cookiecutter.project_dir }}/config/urls.py
+++ {{ cookiecutter.project_dir }}/config/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import include, path
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="homepage.html"), name="home"),
-    path('health/', include('health_check.urls')),
-    path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    path("health/", include("health_check.urls")),
+    path("admin/", admin.site.urls),
 ]
