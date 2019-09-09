4a6e7109a6026fcbbce333d4072dc25b9d5ac23a
sparsonsemoney <50671476+sparsonsemoney@users.noreply.github.com>
2019-09-09 17:48:56 -0400

    Updated to use avo core JWT auth class.

--- {{ cookiecutter.project_dir }}/config/settings.py
+++ {{ cookiecutter.project_dir }}/config/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
-        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+         "avo.core.api.authentication.TrustedUserJSONWebTokenAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
